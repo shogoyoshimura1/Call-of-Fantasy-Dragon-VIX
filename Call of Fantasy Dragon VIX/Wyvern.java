6868415f13b69cb2f12be90b80cc4b7073ff3540
HungryKonpaku <15165@hakodate.kosen-ac.jp>
2019-11-14 09:48:56 +0900

    ワイバーンの動きを追加

--- Call of Fantasy Dragon VIX/Wyvern.java
+++ Call of Fantasy Dragon VIX/Wyvern.java
@@ -1,19 +1,26 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.lang.Math;
 /**
  * Write a description of class Wyvern here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class Wyvern extends ENEMY
 {
+    int a = 0;
+    double b = 0;
+    double c = 0;
     /**
      * Act - do whatever the Wyvern wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
-    }    
+        Math.sin(Math.toRadians(c));
+        move((int)c);
+        c++;
+    }
 }
+
