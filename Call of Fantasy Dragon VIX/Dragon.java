bb31b08f80538c4302f201a9a947ad3c31c77ad8
shogoyoshimura1 <15166@hakodate.kosen-ac.jp>
2019-12-06 09:42:57 +0900

    yoshimura 更新

--- Call of Fantasy Dragon VIX/Dragon.java
+++ Call of Fantasy Dragon VIX/Dragon.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+  import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Dragon here.
@@ -12,8 +12,8 @@ public class Dragon extends ENEMY
      * Act - do whatever the Dragon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
+   public void rotate(double r)
     {
-        // Add your action code here.
-    }    
+
+    }
 }
--- Call of Fantasy Dragon VIX/Wyvern.java
+++ Call of Fantasy Dragon VIX/Wyvern.java
@@ -12,8 +12,5 @@ public class Wyvern extends ENEMY
      * Act - do whatever the Wyvern wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
+       
 }
