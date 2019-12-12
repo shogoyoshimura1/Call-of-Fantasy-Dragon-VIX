c3bbc2990d994fb41a47b47027b84f0d340aa9a8
shogoyoshimura1 <15166@hakodate.kosen-ac.jp>
2019-12-12 09:36:00 +0900

    画像の大きさ変更

--- Call of Fantasy Dragon VIX/Wyvern.java
+++ Call of Fantasy Dragon VIX/Wyvern.java
@@ -16,15 +16,12 @@ public class Wyvern extends ENEMY
      * Act - do whatever the Wyvern wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-<<<<<<< HEAD
-       
-=======
+
     public void act() 
     {
         Math.sin(Math.toRadians(c));
         move((int)c);
         //c++;
     }
->>>>>>> 161c190e05406c7f25ec79303945abb019aed75a
 }
 
